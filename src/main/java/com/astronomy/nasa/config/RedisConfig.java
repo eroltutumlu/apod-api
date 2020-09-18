fd38de48b17b7b55af3a4abcdf0ad74b82d62ee8
Erol <eroltutumlu@gmail.com>
2020-09-18 22:26:17 +0300

    feat: test yml added

--- src/main/java/com/astronomy/nasa/config/RedisConfig.java
+++ src/main/java/com/astronomy/nasa/config/RedisConfig.java
@@ -1,16 +1,26 @@
 package com.astronomy.nasa.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.GenericToStringSerializer;
 
 @Configuration
 public class RedisConfig {
+
+    @Value("${redis.host}")
+    private String redisHostName;
+
+    @Value("${redis.port}")
+    private int redisPort;
+
     @Bean
     JedisConnectionFactory jedisConnectionFactory() {
-        return new JedisConnectionFactory();
+        RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration(redisHostName, redisPort);
+        return new JedisConnectionFactory(redisStandaloneConfiguration);
     }
 
     @Bean
