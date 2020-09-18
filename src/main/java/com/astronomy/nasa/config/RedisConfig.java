21325c6eaa6f5dc702516959a8df889593e13221
Erol <eroltutumlu@gmail.com>
2020-09-18 22:55:04 +0300

    fix: try redis

--- src/main/java/com/astronomy/nasa/config/RedisConfig.java
+++ src/main/java/com/astronomy/nasa/config/RedisConfig.java
@@ -3,10 +3,12 @@ package com.astronomy.nasa.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.GenericToStringSerializer;
+import redis.clients.jedis.Protocol;
+import java.net.URI;
+import java.net.URISyntaxException;
 
 @Configuration
 public class RedisConfig {
@@ -18,13 +20,18 @@ public class RedisConfig {
     private int redisPort;
 
     @Bean
-    JedisConnectionFactory jedisConnectionFactory() {
-        RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration(redisHostName, redisPort);
-        return new JedisConnectionFactory(redisStandaloneConfiguration);
+    JedisConnectionFactory jedisConnectionFactory() throws URISyntaxException {
+        URI redisUri = new URI(redisHostName);
+        JedisConnectionFactory redisConnectionFactory = new JedisConnectionFactory();
+        redisConnectionFactory.setHostName(redisUri.getHost());
+        redisConnectionFactory.setPort(redisUri.getPort());
+        redisConnectionFactory.setTimeout(Protocol.DEFAULT_TIMEOUT);
+        redisConnectionFactory.setPassword(redisUri.getUserInfo().split(":",2)[1]);
+        return redisConnectionFactory;
     }
 
     @Bean
-    public RedisTemplate<String, Object> redisTemplate() {
+    public RedisTemplate<String, Object> redisTemplate() throws URISyntaxException {
         final RedisTemplate<String, Object> redisTemplate = new RedisTemplate<String, Object>();
         redisTemplate.setConnectionFactory(jedisConnectionFactory());
         redisTemplate.setValueSerializer(new GenericToStringSerializer<Object>(Object.class));
